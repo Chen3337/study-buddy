4c07062f67434213e3b247d2e3a40d7be5d23198
zhen yong chen <dannychen111@yahoo.com>
2020-02-23 13:22:48 -0600

    backend passport is working

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,74 @@
 const express = require('express');
 const router = express.Router();
-const ShoppingList = require('../models/shoppinglist');
+const ShoppingList = require('../models/vocab');
+const User = require('../models/passport/user');
+const passport = require('../models/passport/passportuser');
 
-router.get('/shoppinglist', (req, res, next) => {
-    ShoppingList
-        .find({}, 'item')
-        .then(data => res.json(data))
-        .catch(next)
-});
-
-router.post('/shoppinglist', (req, res, next) => {
-    if (req.body.item) {
-        ShoppingList
-            .create(req.body)
-            .then(data => res.json(data))
-            .catch(next);
-    }
-    else {
-        res.json({ error: "The item field is empty" })
-    }
-});
-
-router.delete('/shoppinglist/:id', (req, res, next) => {
-    ShoppingList.findOneAndDelete({ "_id": req.params.id })
-        .then(data => res.json(data))
-        .catch(next)
+router.get('/hello', (req, res) => {
+    res.json([{hello:"hi"}]);
 })
 
-module.exports = router;
+
+router.post('/', (req, res) => {
+    console.log('user signup');
+
+    const { username, password } = req.body
+    // ADD VALIDATION
+    User.findOne({ username: username }, (err, user) => {
+        if (err) {
+            console.log('User.js post error: ', err)
+        } else if (user) {
+            res.json({
+                error: `Sorry, already a user with the username: ${username}`
+            })
+        }
+        else {
+            const newUser = new User({
+                username: username,
+                password: password
+            })
+            newUser.save((err, savedUser) => {
+                if (err) return res.json(err)
+                res.json(savedUser)
+            })
+        }
+    })
+})
+
+router.post(
+    '/login',
+    function (req, res, next) {
+        console.log('routes/user.js, login, req.body: ');
+        console.log(req.body)
+        next()
+    },
+    passport.authenticate('local'),
+    (req, res) => {
+        console.log('logged in', req.user);
+        var userInfo = {
+            username: req.user.username
+        };
+        res.send(userInfo);
+    }
+)
+
+router.get('/', (req, res, next) => {
+    console.log('===== user!!======')
+    console.log(req.user)
+    if (req.user) {
+        res.json({ user: req.user })
+    } else {
+        res.json({ user: null })
+    }
+})
+
+router.post('/logout', (req, res) => {
+    if (req.user) {
+        req.logout()
+        res.send({ msg: 'logging out' })
+    } else {
+        res.send({ msg: 'no user to log out' })
+    }
+})
+
+module.exports = router
