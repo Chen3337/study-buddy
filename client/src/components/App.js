a826653e64de2a3fbd114ac12b4fd5e98d3f9fb2
zhen yong chen <dannychen111@yahoo.com>
2020-03-11 18:47:25 -0500

    navbar in a different folder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,46 +6,27 @@ import Login from "./login/Login";
 import SignUp from "./signup/Signup";
 import Home from "./home/Home";
 import About from "./about/About";
-
+import Navbar from './navbar/navbar';
 
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-sm navbar-light fixed-top" style = {{backgroundColor: "light-gray"}}>
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>Study Buddy</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/home"}>Home </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/about"}>About </Link>
-              </li>
-            </ul>
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/home" component={Home} />
+              <Route path="/about" component={About} />
+            </Switch>
           </div>
         </div>
-      </nav>
-
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-            <Route path="/home" component={Home} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </div>
       </div>
-    </div></Router>
+    </Router>
   );
 }
 
