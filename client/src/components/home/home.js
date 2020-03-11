ab2403c18a9bbd1b37bcac0266d122b8a06e0a77
Joncarrasco98 <carrascoj1998@gmail.com>
2020-03-11 16:47:32 -0500

    made a about page

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-
+import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 export default class Home extends Component {
     apicall = () => {
@@ -10,23 +8,48 @@ export default class Home extends Component {
     }
     render() {
         return (
-<Row className="justify-content-md-center">
-<Col xs lg="12">
-      
-    
-  <Card bg="primary" text="white" style={{ width: '18rem' }}>
-    <Card.Header>Header</Card.Header>
-     <Card.Body>
-       <Card.Title>Primary Card Title</Card.Title>
-       <Card.Text>
-         Some quick example text to build on the card title and make up the bulk
-         of the card's content.
-       </Card.Text>
-    </Card.Body>
-  </Card>
-</Col>
-    
-    </Row>
+  <CardDeck>
+    <Card>
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in to
+         additional content. This content is a little bit longer.
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Footer>
+    </Card>
+    <Card>
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This card has supporting text below as a natural lead-in to additional
+          content.{' '}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Footer>
+    </Card>
+    <Card>
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in to
+          additional content. This card has even longer content than the first to
+          show that equal height action.
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Footer>
+    </Card>
+  </CardDeck>
         );
         }
     }
